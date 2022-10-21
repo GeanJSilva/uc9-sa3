5c7dd43db7a7d116548f1b018a04296e44ccf5aa
Gean J. Silva <109292751+GeanJSilva@users.noreply.github.com>
2022-10-21 15:13:46 -0300

    implementacao login angular

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../views/home/home.component';
+import { LoginComponent } from "../views/login/login.component";
+
+//regras para chamar a pagina quando clicar no menu:
 
 const routes: Routes = [
-  {path:"", component: HomeComponent}
+  {path:"", component: HomeComponent},
+  {path:"login", component: LoginComponent}
 
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
